7c91491b864ddfb3c44531782e077472ee657e07
Augusto <88454824+augusto404@users.noreply.github.com>
2022-02-27 21:49:34 -0300

    condicao simples

--- JavaScript/aula011/ex008.js
+++ JavaScript/aula011/ex008.js
@@ -1,1 +1,9 @@
-console.log('O console funcionou corretamente.')
+var velocidade = 100
+
+console.log(`A velocidade do seu carro é de ${velocidade}Km/h`)
+
+if (velocidade > 60) { // Condição simples.
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija usando sempre o cinto de segurança!`)
