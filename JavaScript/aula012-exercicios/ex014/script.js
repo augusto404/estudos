1b92ecb6393284334f1902ff3806a03ff726cb38
Augusto <88454824+augusto404@users.noreply.github.com>
2022-03-09 16:37:35 -0300

    corrigindo problema de mal funcionamento do ex014

--- JavaScript/aula012-exercicios/ex014/script.js
+++ JavaScript/aula012-exercicios/ex014/script.js
@@ -1,19 +1,19 @@
 function hora_do_dia() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var msg = document.querySelector('div#msg')
+    var img = document.querySelector('div#imagem')
 
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12) {
-        img.src = 'manha_.jpg'
+        img.innerHTML = '<img src="manha_.jpg" alt="manhã">'
         document.body.style.background = '#E0884B'
     } else if (hora >= 12 && hora < 18) {
-        img.src = 'tarde.jpg'
+        img.innerHTML = '<img src="tarde.jpg" alt="tarde">'
         document.body.style.background = '#A5BBC8'
     } else {
-        img.src = 'noite.jpg'
+        img.innerHTML = '<img src="noite.jpg" alt="noite">'
         document.body.style.background = '#013954'
     }
 }
