69f7fa496beca2a601282d309e6c5ea410293f57
Augusto <88454824+augusto404@users.noreply.github.com>
2022-03-09 19:44:32 -0300

    aula13

---
+++ JavaScript/aula013/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var cont = 1
+while (cont <= 6) {
+    console.log(`Linha número ${cont}`)
+    cont++
+}
+*/
+
+var cont = 1
+do {
+    console.log(`Linha número ${cont}`)
+    cont++
+} while (cont <= 6)
