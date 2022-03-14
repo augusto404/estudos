4e3dbbee21a7ed05ab182b589e66b927245f2621
Augusto404 <augustojoao.pessoal@gmail.com>
2022-03-14 20:42:53 -0300

    aula16

---
+++ JavaScript/aula016/funcao002.js
@@ -0,0 +1,5 @@
+function soma(n1, n2=0){ // O parâmetro que adiciona zero serve para a saída não dar NaN.
+    return n1 + n2
+}
+
+console.log(soma(7, 10))
---
+++ JavaScript/aula016/funcao003.js
@@ -0,0 +1,5 @@
+let variavel = function(x){ // A variável contém uma função dentro de si, e irá executar-la quando um valor for atribuído.
+    return x * 2
+}
+
+console.log(variavel(5))
